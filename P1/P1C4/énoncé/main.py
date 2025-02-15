3e705afd7eac7ee299a51b9b433b2ea5a6d5007c
marycanasr <marycanasr@gmail.com>
2025-02-15 16:37:17 +0100

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+nom = "Jean"
+age = 25
+print(f"je m'appelle {nom} et j'ai {age} ans.")
+
+age = age+10 #ou "age+ = 10"
+
+print(f"je m'appelle {nom} et j'ai {age} ans maintenant.")
